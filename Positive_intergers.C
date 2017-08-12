6358cd52a0f26dc08222ac3babd7a89bb47fc99f
boobeshkumar <30952711+boobeshkumar@users.noreply.github.com>
2017-08-12 17:04:38 +0530

    Positive integer or negative integer
    checking wheather the given integer is positive or not

---
+++ Positive_intergers.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int i;
+printf("Enter the value");
+scanf("%d",i);
+if(i>0)
+printf("The given value is positive integer");
+elseif(i<0)
+printf("The given value is negative integer");
+else
+printf("The value is neither positive nor negative");
+}
